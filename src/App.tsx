544caa9434dbeea54bad11dca6829c46ede3f066
Jacob Rogers <rogersair2@aol.com>
2024-01-05 00:46:08 -0500

    SignIn component basic UI setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import { useState } from "react";
 import Test from "./components/Test";
 import NavBar from "./components/NavBar";
+import SignIn from "./components/SignIn";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <main className="w-screen h-screen bg-neutral-500">
-        <NavBar />
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <Test />
+      <main className="grid place-items-center w-screen h-screen bg-neutral-500">
+        <SignIn />
       </main>
     </>
   );
