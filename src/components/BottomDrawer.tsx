f826cf80bdbeea004ceaf0c52242a65369e690f6
Jacob Rogers <rogersair2@aol.com>
2024-04-25 21:36:42 -0400

    styling fixes for editor photo carousel

--- src/components/BottomDrawer.tsx
+++ src/components/BottomDrawer.tsx
@@ -49,16 +49,21 @@ const BottomDrawer = ({
         />
       </DrawerTrigger>
       <DrawerContent className="h-fit p-4 grid place-items-center w-full text-center">
-      <DrawerHeader className="text-center grid grid-rows-2 place-items-center">
-        <DrawerTitle>Your photos</DrawerTitle>
-        <DrawerDescription>Select a photo to add it to your note; you can add and manage your photos in user-settings</DrawerDescription>
-      </DrawerHeader>
+        <DrawerHeader className="text-center grid grid-rows-2 place-items-center">
+          <DrawerTitle>Your photos</DrawerTitle>
+          <DrawerDescription>
+            Select a photo to add it to your note; you can add and manage your
+            photos in user-settings
+          </DrawerDescription>
+        </DrawerHeader>
         <Carousel
           opts={{
             align: "start",
           }}
+          orientation="horizontal"
           className="w-[10rem] md:w-[80%]">
-          <CarouselContent className="">
+          <CarouselContent
+            className={`${files.length <= 0 ? "grid place-items-center" : ""}`}>
             {files.length > 0 ? (
               files.map((file) => (
                 <CarouselItem
@@ -73,13 +78,13 @@ const BottomDrawer = ({
                 </CarouselItem>
               ))
             ) : (
-              <p className="flex justify-center items-center w-auto h-10 p-4 border-solid rounded-md border-black border-2 text-black font-semibold bg-amber-400 col-span-3">
+              <p className="flex justify-center items-center w-auto h-10 p-4 border-solid rounded-md border-black border-2 text-black font-semibold bg-amber-400 col-span-3 self-center justify-self-center">
                 {noFilesMessage}
               </p>
             )}
           </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
+          {files.length > 0 ? <CarouselPrevious /> : null}
+          {files.length > 0 ? <CarouselNext /> : null}
         </Carousel>
       </DrawerContent>
     </Drawer>
